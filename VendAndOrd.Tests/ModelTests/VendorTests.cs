ab2c9b40e5d59bb78ceca216ec712eaabf0932ee
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-24 13:29:54 -0800

    add passing spec that returns the vendor ID, also added teardown method to dispose all Vendors between tests

--- VendAndOrd.Tests/ModelTests/VendorTests.cs
+++ VendAndOrd.Tests/ModelTests/VendorTests.cs
@@ -5,10 +5,15 @@ using System;
 namespace VendAndOrd.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
     [TestMethod]
-    public void VendorConstructor_CreatesInstancesOfCategory_Vendor()
+    public void VendorConstructor_CreatesInstancesOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
@@ -26,6 +31,20 @@ namespace VendAndOrd.Tests
 
       //Assert
       Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      //Arrange
+      string name = "test vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      int result = newVendor.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
     }
   }
 }
