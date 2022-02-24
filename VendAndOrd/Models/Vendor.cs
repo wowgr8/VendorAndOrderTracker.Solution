47b397889728906743852ea3e4448e887a866967
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-24 13:04:58 -0800

    add using statement, declared namespaces, class, properties, and constructor

---
+++ VendAndOrd/Models/Vendor.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace VendAndOrd.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor>{};
+    public string Name { get; set; }
+    
+
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+    }
+  }
+}
