6f7f94547a31ed9816093b01d4cc577b460c801a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-24 13:12:41 -0800

    add passing spec that retrieves vendor names

--- VendAndOrd.Tests/ModelTests/VendorTests.cs
+++ VendAndOrd.Tests/ModelTests/VendorTests.cs
@@ -13,5 +13,19 @@ namespace VendAndOrd.Tests
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "test vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
   }
 }
