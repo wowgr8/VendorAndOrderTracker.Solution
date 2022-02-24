19e6206671b959c3ebe48c4d6facf361033dbd5d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-24 13:03:38 -0800

    add passing spec testing Vendor constructor can create instance of vendor

--- VendAndOrd.Tests/ModelTests/VendorTests.cs
+++ VendAndOrd.Tests/ModelTests/VendorTests.cs
@@ -2,8 +2,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendAndOrd.Models;
 using System;
 
-namespace VendAndOrd.TestTools
+namespace VendAndOrd.Tests
 {
   [TestClass]
   public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstancesOfCategory_Vendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
 }
