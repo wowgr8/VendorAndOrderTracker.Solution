896604733c0a6e2ea795f395d9d89f1c491aed3e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-24 15:50:40 -0800

    add standard controller setup and index category to display all Vendors

---
+++ VendAndOrd/Controllers/VendorsController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendAndOrd.Models;
+
+namespace VendAndOrd.Controllers
+{
+  public class VendorsControllers : Controller 
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> fullListVendors = Vendor.GetAll();
+      return View(fullListVendors);
+    }
+  }
+}
