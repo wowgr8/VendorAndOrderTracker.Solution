8e5395125d08dd0f32912060b85c38f8534d7cf0
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-25 15:25:11 -0800

    add Show route

--- VendAndOrd/Controllers/VendorsController.cs
+++ VendAndOrd/Controllers/VendorsController.cs
@@ -26,5 +26,18 @@ namespace VendAndOrd.Controllers
       Vendor newVendor = new Vendor(vendorName);
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders:
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return  View(model);
+    }
+
+    
   }
 }
