85f55f0b1e28ace1d340bc55f4ecd11a70ffd2e8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-24 17:06:47 -0800

    add constructor

--- VendAndOrd/Models/Order.cs
+++ VendAndOrd/Models/Order.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace VendAndOrd.Models
 {
   public class Order
   {
+    public string Description { get; set; }
+    
+    public Order(string description)
+    {
+      Description = description;
+      
+    }
 
   }
 } 
