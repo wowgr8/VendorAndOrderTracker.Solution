ae231fa9a080d1ca07bdd07b5b3ba6e2b264e5af
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-25 15:39:47 -0800

    add missing semicolon

--- VendAndOrd/Controllers/VendorsController.cs
+++ VendAndOrd/Controllers/VendorsController.cs
@@ -32,7 +32,7 @@ namespace VendAndOrd.Controllers
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor selectedVendor = Vendor.Find(id);
-      List<Order> vendorOrders = selectedVendor.Orders:
+      List<Order> vendorOrders = selectedVendor.Orders;
       model.Add("vendor", selectedVendor);
       model.Add("orders", vendorOrders);
       return  View(model);
