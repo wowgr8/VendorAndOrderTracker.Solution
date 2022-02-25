1f8d9e01826f2b00d18d43f8d7219169816753c8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-25 13:59:26 -0800

    fixed typo in controller and file name for continuity

--- VendAndOrd/Controllers/OrdersController.cs
+++ VendAndOrd/Controllers/OrdersController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace VendAndOrd.Controllers
 {
-  public class OrderController : Controller
+  public class OrdersController : Controller
   {
 
     [HttpGet("/vendors/{vendorId}/orders/new")]
