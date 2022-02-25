119906c22776e9a76fb788770abdfbd2b52fca90
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-24 19:32:22 -0800

    add teardown method to dispose all Orders between tests, also add passing spec for returning empty list of Orders

--- VendAndOrd.Tests/ModelTests/OrderTests.cs
+++ VendAndOrd.Tests/ModelTests/OrderTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendAndOrd.Models;
+using System;
+using System.Collections.Generic;
 
 namespace VendAndOrd.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
 
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
@@ -40,6 +46,16 @@ namespace VendAndOrd.Tests
       Assert.AreEqual(updatedOrder, result);
     }
 
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      //Arrange
+      List<Order> newList = new List<Order> {};
+      //Act
+      List<Order> result = Order.GetAll();
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
 
   }
 }
