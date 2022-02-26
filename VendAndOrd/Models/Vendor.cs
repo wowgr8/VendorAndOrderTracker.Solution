0606d9bf226f1a4c01e6c6997061173d320f9f95
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-26 12:30:18 -0800

    add VenDescription to class and constructor

--- VendAndOrd/Models/Vendor.cs
+++ VendAndOrd/Models/Vendor.cs
@@ -6,12 +6,14 @@ namespace VendAndOrd.Models
   {
     private static List<Vendor> _instances = new List<Vendor>{};
     public string Name { get; set; }
+    public string VenDescription { get; set; }
     public int Id { get; }
     public List<Order> Orders { get; set; }
 
-    public Vendor(string vendorName)
+    public Vendor(string vendorName, string vendorDesc)
     {
       Name = vendorName;
+      VenDescription = vendorDesc;
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
