6d1ccc3bd65cf3f601715a482b82f1ee1fc4289a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-26 13:21:45 -0800

    add order deliverydate and price parameters to Create route

--- VendAndOrd/Controllers/VendorsController.cs
+++ VendAndOrd/Controllers/VendorsController.cs
@@ -39,11 +39,11 @@ namespace VendAndOrd.Controllers
     }
 
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderDescription)
+    public ActionResult Create(int vendorId, string orderDescription, string orderDeliveryDate, int orderPrice)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderDescription);
+      Order newOrder = new Order(orderDescription, orderDeliveryDate, orderPrice);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
